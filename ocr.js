9b1b12d50ac86249b0c692706c5e439531979c04
plucian22 <36382002+plucian22@users.noreply.github.com>
2023-07-08 15:13:42 +0300

    Update ocr.js

--- ocr.js
+++ ocr.js
@@ -1,2 +1,46 @@
+//Initate Tesseract model wasm using worker:
 
-
+
+
+//Glob variable OCR worker:
+const worker = Tesseract.createWorker({
+  logger: m => console.log(m)
+});
+Tesseract.setLogging(true);
+
+
+
+//Initiate Tesseract worker:
+async function Init()
+{
+  //console.log('Initiate worker')
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  
+  //Recognize only phone numbers:
+  await worker.setParameters({ tessedit_char_whitelist: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',});
+  
+  //Enable start button:
+  enableWebcamButton.classList.remove('invisible');
+  enableWebcamButton.innerHTML = 'Start camera';
+  console.log('Finished loading tesseract');
+}
+
+
+
+
+//Function perform OCR on image
+async function Recognize(image)
+{
+  let result = await worker.recognize(image);
+  console.log(result.data.text);
+  console.log('Finished recognizing');
+  return result.data.text;
+}
+
+//Function terminates the worker:
+async function ShutDownWorker()
+{
+  await worker.terminate();
+}
